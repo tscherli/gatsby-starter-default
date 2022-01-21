acc424dc19332a0597c174b7fb80a377f76b7df8
tscherli <87180788+tscherli@users.noreply.github.com>
2022-01-21 11:31:21 +0100

    Test name

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>My name is your name.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
